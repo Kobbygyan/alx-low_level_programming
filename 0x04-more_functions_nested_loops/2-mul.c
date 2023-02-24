b96465f14c5ba131e52fa6268488bbdec3967f55
Kobbygyan <kobbygyan86@gmail.com>
2023-02-23 21:43:05 -0800

    More loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mu1 - multiply 2 int
+ * @a: first int
+ * @b: second int
+ * Return: addition of the multiplicattion
+ */
+
+int mu1( int a, int b)
+{
+	return (a * b);
+}
