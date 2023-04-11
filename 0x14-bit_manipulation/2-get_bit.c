c1bf0039dc32c3079a90d9f59dd610c8547375da
Kobbygyan <kobbygyan86@gmail.com>
2023-04-10 19:12:33 -0700

    bit-m3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_bit - Returns the value of the bit at the given index in a binary number.
+ * get_bit - Returns the value of the bit at the given index in a binary number
  * @n: The binary number whose bit value is to be retrieved
- * @index: The index of the bit to be retrieved, starting from the least significant bit at index 0
+ * @index: The index of the bit to be retrieved
  *
  * Return: The value of the bit at the given index (0 or 1)
  */
